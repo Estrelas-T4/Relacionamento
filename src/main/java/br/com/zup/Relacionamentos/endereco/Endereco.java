e4552ee979fed4690da9c1d691cf4957964e2904
Vinícius <vinicius.raymundo@zup.com.br>
2021-11-16 11:05:29 -0300

    Adicionar strategy do id

--- src/main/java/br/com/zup/Relacionamentos/endereco/Endereco.java
+++ src/main/java/br/com/zup/Relacionamentos/endereco/Endereco.java
@@ -1,14 +1,13 @@
 package br.com.zup.Relacionamentos.endereco;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+
+import javax.persistence.*;
 
 @Entity
 @Table(name = "enderecos")
 public class Endereco {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @Column(nullable = false)
     private String locradouro;
