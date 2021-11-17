c40152d289699461574c9e0494189a542b02261e
Vinícius <vinicius.raymundo@zup.com.br>
2021-11-17 16:20:04 -0300

    Correcao do controller advice

--- src/main/java/br/com/zup/Relacionamentos/config/ControllerAdivisor.java
+++ src/main/java/br/com/zup/Relacionamentos/config/ControllerAdivisor.java
@@ -2,21 +2,25 @@ package br.com.zup.Relacionamentos.config;
 
 import br.com.zup.Relacionamentos.cadastro.custom_exceptions.CadastroNaoEncontradoException;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ControllerAdivisor {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-    public HashMap<String, String> manipularValidacao(MethodArgumentNotValidException exception){
-        HashMap<String, String> json = new HashMap<>();
-        exception.getFieldErrors().stream().map(erro -> json.put(erro.getField(), erro.getDefaultMessage()));
+    public Map<Object, Object> manipularValidacao(MethodArgumentNotValidException exception){
+        Map<Object, Object> json =
+                exception.getFieldErrors().stream().collect(Collectors.toMap(m -> m.getField(), m -> m.getDefaultMessage() ));
+
 
         return json;
     }
